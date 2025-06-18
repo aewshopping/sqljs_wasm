fefd1899592301730c345594fc04645cded1df13
aewshopping <aewshopping@gmail.com>
2025-06-18 12:55:19 +0100

    Update csvSources.js
    formatting and setting up inputs a bit more realistically

--- js/csvSources.js
+++ js/csvSources.js
@@ -1,9 +1,25 @@
 const fileSources = [
-    { url: 'https://raw.githubusercontent.com/aewshopping/history_books/refs/heads/main/data_csv/books-tags.csv', type: 'csv', tableName: 'BookTags_Custom' },
-    { url: 'https://raw.githubusercontent.com/aewshopping/history_books/refs/heads/main/data_csv/cats.csv', type: 'csv' }, // No tableName, should default
-    { url: 'https://raw.githubusercontent.com/aewshopping/history_books/refs/heads/main/data_csv/popular-history-books.csv', type: 'csv', tableName: 'PopularHistoryBooks' },
-    { url: 'https://raw.githubusercontent.com/aewshopping/history_books/refs/heads/main/data_csv/tags.csv', type: 'csv', tableName: 'All_Tags' },
-    { url: 'https://raw.githubusercontent.com/aewshopping/history_books/refs/heads/main/data_tsv/quotes.tsv', type: 'tsv', tableName: 'Quotes_TSV' }
+    {
+        url: 'https://raw.githubusercontent.com/aewshopping/history_books/refs/heads/main/data_csv/books-tags.csv',
+        type: 'csv'
+    },
+    {
+        url: 'https://raw.githubusercontent.com/aewshopping/history_books/refs/heads/main/data_csv/cats.csv',
+        type: 'csv'
+    },
+    {
+        url: 'https://raw.githubusercontent.com/aewshopping/history_books/refs/heads/main/data_csv/popular-history-books.csv',
+        type: 'csv',
+        tableName: 'books'
+    },
+    {
+        url: 'https://raw.githubusercontent.com/aewshopping/history_books/refs/heads/main/data_csv/tags.csv',
+        type: 'csv'
+    },
+    {
+        url: 'https://raw.githubusercontent.com/aewshopping/history_books/refs/heads/main/data_tsv/quotes.tsv',
+        type: 'tsv'
+    }
 ];
 
 export { fileSources };
